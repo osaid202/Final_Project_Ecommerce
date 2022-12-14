7f9c112ad20584df6e974390f6025f0fdc139b34
osaid202 <osaid202@gmail.com>
2022-12-14 09:06:33 -0500

    activeAdmin Category + products parm

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,15 +1,15 @@
-ActiveAdmin.register Product do
+ActiveAdmin.register Category do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :price, :stock, :category_id
+  permit_params :name
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name, :price, :stock]
+  #   permitted = [:name]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
