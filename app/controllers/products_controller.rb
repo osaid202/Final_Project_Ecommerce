a3f242394c0575058c38342cbad9b1c9b583f434
osaid202 <osaid202@gmail.com>
2022-12-14 13:42:46 -0500

    Hierarchical Search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < InheritedResources::Base
   def index
-    @products = Product.search(params[:search])
+    @products = Product.search(params[:search], params[:category])
     @products = @products.order(:created_at).page(params[:page])
+    @categories = Category.distinct.pluck(:name)
   end
 
   private
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,16 @@ class Product < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :orders
 
-  def self.search(search)
-    if search
-       where(["name LIKE ?", "%#{search}%"])
-      #where(name: search)
+  def self.search(search, category)
+    if !search.to_s.strip.empty? && !category.to_s.strip.empty?
+      # where(name: search)
+      where(["name LIKE ? and category_id= ?", "%#{search}%",
+             Category.where(name: category).first.id])
+    elsif !search.to_s.strip.empty?
+      where(["name LIKE ?", "%#{search}%"])
+
+    elsif !category.to_s.strip.empty?
+      where(category_id: Category.where(name: category).first.id)
     else
       all
     end
