0e8e024f4e2a6ece783019cdab9c3372dd44ad17
osaid202 <osaid202@gmail.com>
2022-12-15 09:47:26 -0500

    Multiple products for each order seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,20 @@ require "faker"
   category = Category.create!(name: Faker::Commerce.unique.department)
 
   10.times do
-    last_price = Faker::Number.decimal(l_digits: 2)
-    product = Product.create!(category_id: category.id, name:  Faker::Commerce.unique.product_name,
-                              price: last_price,
-                              stock: Faker::Number.number(digits: 3))
-
-    order = customer.orders.create!(product_id: product.id, date: Date.today, price: last_price,
+    # product1_price = Faker::Number.decimal(l_digits: 2)
+    product1 = Product.create!(category_id: category.id, name:  Faker::Commerce.unique.product_name,
+                               price: Faker::Number.decimal(l_digits: 2),
+                               stock: Faker::Number.number(digits: 3))
+    product2 = Product.create!(category_id: category.id, name:  Faker::Commerce.unique.product_name,
+                               price: Faker::Number.decimal(l_digits: 2),
+                               stock: Faker::Number.number(digits: 3))
+    # order = customer.orders.create!(date: Date.today, grandTotal: product1_price,
+    #                                 quantity: Faker::Number.number(digits: 1))
+    taxes = ((product1.price + product2.price) * 0.11).round(2)
+    order = product1.orders.create!(customer_id: customer.id, taxes: taxes, grandTotal: product1.price + product2.price + taxes,
                                     quantity: Faker::Number.number(digits: 1))
+    order.products << product2
+    # order.product = product
   end
 
   # OR
