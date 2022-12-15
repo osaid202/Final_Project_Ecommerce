766877546b7c79fcb4768806cf58073c2678d60b
osaid202 <osaid202@gmail.com>
2022-12-15 13:59:56 -0500

    Flash notice

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < InheritedResources::Base
   def index
     @products = Product.search(params[:product], params[:category])
+    if @products.empty?
+      flash.now[:notice] = "Your search options show 0 results, try searching for 'shoes' and/or
+      'clothing' category."
+    end
     @products = @products.order(:created_at).page(params[:page])
     @categories = Category.distinct.pluck(:name)
   end
